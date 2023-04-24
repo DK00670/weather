f46b3672406c3e8b13af00823cd943dc0f7a30c9
DK00670 <115625395+DK00670@users.noreply.github.com>
2023-04-24 21:23:00 +0100

    Update settings.py

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 from django.conf import settings
 import os
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,3 +127,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS = {
+    messages.SUCCESS: 'is-success',
+    messages.ERROR: 'is-danger',
+}
